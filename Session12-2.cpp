8db8ca1328a7963b550f049d9bc020261e0c08fe
dai611351 <dai611351@gmail.com>
2024-12-10 14:41:53 +0700

    bai2

---
+++ Session12-2.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void inMang(int arr[], int n) {
+    printf("Các phan tu trong mang la: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Nhap so luong phan tu cua mang: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Nhap cac phan tu cua mang:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Nhap phan tu %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    inMang(arr, n);
+
+    return 0;
+}
+
